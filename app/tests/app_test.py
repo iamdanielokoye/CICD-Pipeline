8d012abe7338a14eba1b377a12a9edcff0735f4e
iamdanielokoye <okoyedann@gmail.com>
2025-01-28 09:34:16 +0100

    Replace unittest test file with a new test implementation and configure VSCode settings for unittest

--- app/tests/app_test.py
+++ app/tests/app_test.py
@@ -3,19 +3,12 @@ import unittest
 from app import app
 
 
-
-
-
 class TestApp(unittest.TestCase):
 
     def test_hello(self):
-
         tester = app.test_client(self)
-
         response = tester.get('/')
-
         self.assertEqual(response.status_code, 200)
-
         self.assertEqual(response.data, b"Hello, World!")
 
 
